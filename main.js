2f12f282d6f3b0f5658931d8117e76433b5cf156
Shelby-Jean <shelby.gottschall@gmail.com>
2020-07-05 15:35:56 -0400

    updated GUI

--- main.js
+++ main.js
@@ -34,11 +34,17 @@ const printBoard = () => {
 
 const horizontalWin = () => {
   if(board[0][0] === board[0][1] && board[0][1] === board[0][2]) {
-    return true;
+    // if(board[0][0] === 'X' || board[0][0] === 'O') {
+      return true;
+    // }
   } else if(board[1][0] === board[1][1] && board[1][1] === board[1][2]) {
-    return true;
+    // if(board[1][0] === 'X' || board[1][0] === 'O') {
+      return true;
+    // }
   } else if(board[2][0] === board[2][1] && board[2][1] === board[2][2]) {
-    return true;
+    // if(board[2][0] === 'X' || board[2][0] === 'O') {
+      return true;
+    // }
   } else {
     return false;
   }
@@ -46,11 +52,17 @@ const horizontalWin = () => {
 
 const verticalWin = () => {
   if(board[0][0] === board[1][0] && board[1][0] === board[2][0]) {
+    // if(board[0][0] === 'X' || board[0][0] === 'O') {
     return true;
+    // }
   } else if(board[0][1] === board[1][1] && board[1][1] === board[2][1]) {
-    return true;
+    // if(board[0][1] === 'X' || board[0][1] === 'O') {
+      return true;
+    // }
   } else if(board[0][2] === board[1][2] && board[1][2] === board[2][2]) {
-    return true;
+    // if(board[0][2] === 'X' || board[0][2] === 'O') {
+      return true;
+    // }
   } else {
     return false;
   }
@@ -58,9 +70,13 @@ const verticalWin = () => {
 
 const diagonalWin = () => {
   if(board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-    return true;
+    // if(board[0][0] === 'X' || board[0][0] === 'O') {
+      return true;
+    // }
   } else if(board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-    return true;
+    // if(board[0][2] === 'X' || board[0][2] === 'O') {
+      return true;
+    // }
   } else {
     return false;
   }
